b7d16fe30c60b7d86c00b3f03471e823260e23ae
Arik Liber <leonliber@gmail.com>
2017-05-15 13:34:55 +0300

    Notifications placeholder

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="notifications">
-    <i class="el-icon-information"></i> Notifications
-  </div>
+  <section id="notifications">
+    <h3>No Notifications</h3>
+    <img src="../assets/no-notifications.png"/>
+  </section>
 </template>
 
 <script>
@@ -14,5 +15,13 @@ export default {
 #notifications {
   background: #F2EEF5;
   border-right: 1px solid #DBC9EA;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 70px;
+}
+h3 {
+  color: #bfaad2;
+  font-weight: normal;
 }
 </style>
